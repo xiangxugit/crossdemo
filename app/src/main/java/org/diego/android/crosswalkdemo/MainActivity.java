c7e8c0b4bc553a3ff5ba849b738db00c62459001
xiangxugit <568173020@qq.com>
2018-09-17 17:23:05 +0800

    crossdemo
    crossdemo

---
+++ app/src/main/java/org/diego/android/crosswalkdemo/MainActivity.java
@@ -0,0 +1,31 @@
+package org.diego.android.crosswalkdemo;
+
+import android.os.Bundle;
+
+import org.xwalk.core.XWalkActivity;
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+
+public class MainActivity extends XWalkActivity {
+    private XWalkView xWalkWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+
+        // turn on debugging
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+    }
+
+    @Override
+    protected void onXWalkReady() {
+        // Do anything with embedding API
+        xWalkWebView.load("http://www.yisen-wh.com/mygui_terminal/index.html#/home", null);
+    }
+
+}
